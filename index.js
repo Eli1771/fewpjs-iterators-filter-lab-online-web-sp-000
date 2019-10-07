de57c2f715558b7090a357d7cf898de5a35cb1cd
elijahmm17 <elijahmm17@gmail.com>
2019-10-07 14:10:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function findMatching(drivers, name) {
 }
  
 function fuzzyMatch(drivers, name) {
-   
+  return drivers.filter(driver => {
+    return driver.slice(0, name.length) === name;
+  });
 }
  
 function matchName(drivers, name) {
